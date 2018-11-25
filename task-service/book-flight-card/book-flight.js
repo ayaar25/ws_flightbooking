3d1b294056e668abec1a7690b4451ac77506e47b
Husnulzaki Wibisono Haryadi <zackhardyz@gmail.com>
2018-11-25 20:15:53 +0700

    Delete comment

--- task-service/book-flight-card/book-flight.js
+++ task-service/book-flight-card/book-flight.js
@@ -3,24 +3,14 @@ const { Variables } = require('camunda-external-task-client-js');
 const http = require('http');
 const request = require('request');
 
-// configuration for the Client:
-//  - 'baseUrl': url to the Process Engine
-//  - 'logger': utility to automatically log important events
 const config = { baseUrl: 'http://localhost:8080/engine-rest', use: logger };
 
-// create a Client instance with custom configuration
 const client = new Client(config);
 const localVariables = new Variables();
 
-// subscribe to the topic: 'book-flight-card'
 client.subscribe('book-flight-card', async function({ task, taskService }) {
-  // Put your business logic here
 
-  // Get a process variable
   const booking_id = task.variables.get('booking_id');
-
-  console.log('receive bookings request');
-  console.log('booking_id: ' + booking_id);
 
   request('http://localhost:8000/bookings/'+booking_id, { json: true }, (err, res, body) => {
     if (err) { return console.log(err); }
@@ -30,9 +20,6 @@ client.subscribe('book-flight-card', async function({ task, taskService }) {
 
     var flightclass = booking_data.flightclass;
 
-    console.log('scheduleid: ' + scheduleid);
-    console.log('flightclass: ' + flightclass);
-    // Check if seat is available
     request('http://localhost:8000/schedules/'+scheduleid, { json: true }, (err, res, body) => {
       if (err) { return console.log(err); }
       var schedule_data = body.data;
